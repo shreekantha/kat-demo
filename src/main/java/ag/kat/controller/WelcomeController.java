1d17ced37bc673a50f5e56ef4010ca133ddd9e16
Shreekantha <shreekanth.karikalli@outlook.com>
2015-10-03 14:28:31 +0530

    scan

--- src/main/java/ag/kat/controller/WelcomeController.java
+++ src/main/java/ag/kat/controller/WelcomeController.java
@@ -1,12 +1,13 @@
 package ag.kat.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
-	@RequestMapping("/welcome")
+	@RequestMapping(value={"/","/welcome"},method=RequestMethod.GET)
 	public String welcome(){
 		return "Welcome to heroku";
 	}
--- src/main/java/demo/KatDemoApplication.java
+++ src/main/java/demo/KatDemoApplication.java
@@ -2,8 +2,10 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("ag.kat")
 public class KatDemoApplication {
 
     public static void main(String[] args) {
